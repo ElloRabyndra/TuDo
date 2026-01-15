1c18994be3d40ca00eb7550418a490814c3a4b39
ElloRabyndra <Ellorabyndra123@gmail.com>
2026-01-15 09:32:20 +0700

    chore: update db, api end documentation file

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -3,7 +3,7 @@ import { Platform } from "react-native";
 
 const BASE_URL =
   Platform.OS === "android"
-    ? "http://192.168.100.9:8080/api"
+    ? "http://10.165.95.1:8080/api" // Replace with your IP address
     : "http://localhost:8080/api";
 
 const api = axios.create({
